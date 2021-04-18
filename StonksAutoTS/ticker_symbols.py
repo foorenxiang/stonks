008618e953a4ab3f8c57f41e8bb8a32a83b1cd0b
Ren Xiang <foorenxiang@gmail.com>
2021-04-18 14:18:53 +0800

    added ticker module

---
+++ StonksAutoTS/ticker_symbols.py
@@ -0,0 +1,20 @@
+def get_ticker_symbols():
+    ticker_symbols = list(
+        {
+            "PLTR",
+            "TIGR",
+            "TBLT",
+            "GME",
+            "SQ",
+            "WMT",
+            "V",
+            "AAPL",
+            "DIS",
+            "OCGN",
+            "NVAX",
+            "MZH.SI",
+            "D01.SI",
+        }
+    )
+    ticker_symbols.sort()
+    return ticker_symbols
