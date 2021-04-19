0b86b57d4f207f9074a6b7ceb0d5e1314f1f7a70
Ren Xiang <foorenxiang@gmail.com>
2021-04-19 23:34:13 +0800

    refactor into small functions

---
+++ StonksAutoTS/AutoTSConfigs.py
@@ -0,0 +1,42 @@
+from autots import AutoTS
+import math
+
+
+class AutoTSConfigs:
+    FAST = "FAST"
+    ACCURATE = "ACCURATE"
+    FORECAST_LENGTH = 7
+
+    __suggested_validation_trials = math.floor(FORECAST_LENGTH / 3)
+    validation_trials = (
+        __suggested_validation_trials if __suggested_validation_trials > 2 else 2
+    )
+
+    __autots_mode_configs = {
+        "FAST": lambda: AutoTS(
+            forecast_length=AutoTSConfigs.FORECAST_LENGTH,
+            frequency="infer",
+            prediction_interval=0.9,
+            ensemble=None,
+            model_list="superfast",
+            transformer_list="fast",
+            max_generations=5,
+            num_validations=2,
+            validation_method="backwards",
+        ),
+        "ACCURATE": lambda: AutoTS(
+            forecast_length=AutoTSConfigs.FORECAST_LENGTH,
+            frequency="infer",
+            prediction_interval=0.9,
+            ensemble="all",
+            model_list="parallel",
+            transformer_list="fast",
+            max_generations=50,
+            num_validations=AutoTSConfigs.validation_trials,
+            validation_method="backwards",
+        ),
+    }
+
+    @classmethod
+    def create_model_lambda(cls, selectedMode):
+        return cls.__autots_mode_configs[getattr(cls, selectedMode)]
--- StonksAutoTS/stonks_autots.py
+++ StonksAutoTS/stonks_autots.py
@@ -1,24 +1,26 @@
-from datetime import datetime, date
 import logging
-import yfinance as yf
-from joblib import dump
-from autots import AutoTS
-from pathlib import Path
-from datetime import datetime, timedelta
-import math
-import time
+
 
 logger = logging.getLogger(__name__)
 logger.setLevel(logging.INFO)
 
 
+import time
+
+
 def time_exec(lambdaFunc):
-    import time
 
     start_time = time.monotonic()
     lambdaFunc()
     end_time = time.monotonic()
     print(f"Time taken to train: {end_time - start_time}")
+
+
+import yfinance as yf
+from joblib import dump
+from pathlib import Path
+from datetime import datetime, timedelta
+from AutoTSConfigs import AutoTSConfigs
 
 
 def get_stocks():
@@ -53,44 +55,82 @@ def set_date_on_yf_df(yf_df):
     return yf_df
 
 
-class AutoTSConfigs:
-    FAST = "FAST"
-    ACCURATE = "ACCURATE"
-    FORECAST_LENGTH = 7
-
-    __suggested_validation_trials = math.floor(FORECAST_LENGTH / 3)
-    validation_trials = (
-        __suggested_validation_trials if __suggested_validation_trials > 2 else 2
+def train_model_close(selectedMode, target, ticker_name, ticker_dfs):
+    model_close = AutoTSConfigs.create_model_lambda(selectedMode)().fit(
+        ticker_dfs,
+        date_col="DateCol",
+        value_col="Close",
     )
 
-    __autots_mode_configs = {
-        "FAST": lambda: AutoTS(
-            forecast_length=AutoTSConfigs.FORECAST_LENGTH,
-            frequency="infer",
-            prediction_interval=0.9,
-            ensemble=None,
-            model_list="superfast",
-            transformer_list="fast",
-            max_generations=5,
-            num_validations=2,
-            validation_method="backwards",
-        ),
-        "ACCURATE": lambda: AutoTS(
-            forecast_length=AutoTSConfigs.FORECAST_LENGTH,
-            frequency="infer",
-            prediction_interval=0.9,
-            ensemble="dist",
-            model_list="parallel",
-            transformer_list="fast",
-            max_generations=5,
-            num_validations=AutoTSConfigs.validation_trials,
-            validation_method="backwards",
-        ),
+    prediction_close = model_close.predict()
+    # Print the details of the best model
+    print("Details of best model:")
+    print(model_close)
+
+    # point forecasts dataframe
+    forecasts_close = prediction_close.forecast
+    # upper and lower forecasts
+    forecasts_up, forecasts_low = (
+        prediction_close.upper_forecast,
+        prediction_close.lower_forecast,
+    )
+
+    # accuracy of all tried model results
+    model_results_close = model_close.results()
+    # and aggregated from cross validation
+    validation_results_close = model_close.results("validation")
+
+    variables_to_be_saved = {
+        "model_close": model_close,
+        "prediction_close": prediction_close,
+        "forecasts_close": forecasts_close,
+        "model_results_close": model_results_close,
+        "validation_results_close": validation_results_close,
     }
 
-    @classmethod
-    def create_model_lambda(cls, selectedMode):
-        return cls.__autots_mode_configs[getattr(cls, selectedMode)]
+    [
+        dump(var, f"{target}/{f'{ticker_name}_{name}'}.joblib")
+        for name, var in variables_to_be_saved.items()
+    ]
+
+
+def train_model_open(selectedMode, target, ticker_name, ticker_dfs):
+    model_open = AutoTSConfigs.create_model_lambda(selectedMode)().fit(
+        ticker_dfs,
+        date_col="DateCol",
+        value_col="Open",
+    )
+
+    prediction_open = model_open.predict()
+    # Print the details of the best model
+    print("Details of best model:")
+    print(model_open)
+
+    # point forecasts dataframe
+    forecasts_open = prediction_open.forecast
+    # upper and lower forecasts
+    forecasts_up, forecasts_low = (
+        prediction_open.upper_forecast,
+        prediction_open.lower_forecast,
+    )
+
+    # accuracy of all tried model results
+    model_results_open = model_open.results()
+    # and aggregated from cross validation
+    validation_results_open = model_open.results("validation")
+
+    variables_to_be_saved = {
+        "model_open": model_open,
+        "prediction_open": prediction_open,
+        "forecasts_open": forecasts_open,
+        "model_results_open": model_results_open,
+        "validation_results_open": validation_results_open,
+    }
+
+    [
+        dump(var, f"{target}/{f'{ticker_name}_{name}'}.joblib")
+        for name, var in variables_to_be_saved.items()
+    ]
 
 
 # TODO: refactor to make this function smaller (consider using class)
@@ -100,6 +140,7 @@ def train_stonks():
     ticker_dfs = get_stocks_data(stocks)
 
     selectedMode = AutoTSConfigs.FAST
+    print(f"Training with {selectedMode} mode")
 
     target_name = f"{str(datetime.now()).split('.')[0]} {selectedMode}"
     target_parent = Path(__file__).resolve().parent
@@ -111,77 +152,21 @@ def train_stonks():
 
         ticker_dfs = set_date_on_yf_df(ticker_dfs)
 
-        model_close = AutoTSConfigs.create_model_lambda(selectedMode)().fit(
-            ticker_dfs,
-            date_col="DateCol",
-            value_col="Close",
-        )
-
-        prediction_close = model_close.predict()
-        # Print the details of the best model
-        print("Details of best model:")
-        print(model_close)
-
-        # point forecasts dataframe
-        forecasts_close = prediction_close.forecast
-        # upper and lower forecasts
-        forecasts_up, forecasts_low = (
-            prediction_close.upper_forecast,
-            prediction_close.lower_forecast,
-        )
-
-        # accuracy of all tried model results
-        model_results_close = model_close.results()
-        # and aggregated from cross validation
-        validation_results_close = model_close.results("validation")
-
-        model_open = AutoTSConfigs.create_model_lambda(selectedMode)().fit(
-            ticker_dfs,
-            date_col="DateCol",
-            value_col="Open",
-        )
-
-        prediction_open = model_close.predict()
-        # Print the details of the best model
-        print("Details of best model:")
-        print(model_close)
-
-        # point forecasts dataframe
-        forecasts_open = prediction_open.forecast
-        # upper and lower forecasts
-        forecasts_up, forecasts_low = (
-            prediction_open.upper_forecast,
-            prediction_open.lower_forecast,
-        )
-
-        # accuracy of all tried model results
-        model_results_open = model_close.results()
-        # and aggregated from cross validation
-        validation_results_open = model_close.results("validation")
-
-        variables_to_be_saved = {
-            "model_close": model_close,
-            "prediction_close": prediction_close,
-            "forecasts_close": forecasts_close,
-            "model_results_close": model_results_close,
-            "validation_results_close": validation_results_close,
-            "model_open": model_open,
-            "prediction_open": prediction_open,
-            "forecasts_open": forecasts_open,
-            "model_results_open": model_results_open,
-            "validation_results_open": validation_results_open,
-        }
-
-        [
-            dump(var, f"{target}/{f'{ticker_name}_{name}'}.joblib")
-            for name, var in variables_to_be_saved.items()
-        ]
+        train_model_open(selectedMode, target, ticker_name, ticker_dfs)
+        train_model_close(selectedMode, target, ticker_name, ticker_dfs)
 
 
 if __name__ == "__main__":
+    import math
+    import time
+
     start_time = time.monotonic()
     train_stonks()
     end_time = time.monotonic()
     time_taken = end_time - start_time
     mins, secs = math.floor(time_taken / 60), math.floor(time_taken % 60)
     print(f"Time taken to train: {mins}mins {secs}secs")
+    from cli_read_results import AutoTSData
+
+    AutoTSData.print_autots_data(AutoTSData.FORECASTS_OPEN)
+    AutoTSData.print_autots_data(AutoTSData.FORECASTS_CLOSE)
