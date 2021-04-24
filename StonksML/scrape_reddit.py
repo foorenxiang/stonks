f476043127e63f5c7ae7890a0ee6a4539b900653
Foo Ren Xiang <foorenxiang@gmail.com>
2021-04-24 22:16:39 +0800

    added flair filter

--- StonksML/scrape_reddit.py
+++ StonksML/scrape_reddit.py
@@ -35,21 +35,28 @@ def get_save_directory(datasets_loc="datasets", reddit_dump_loc="reddit_dump"):
 
 
 class ScrapeReddit:
-
     save_directory = get_save_directory()
     MAX_POSTS_PER_SUBREDDIT = 100
     subreddit_list = []
+    flair_filters = []
+    __posts = {}
 
     @classmethod
     def config(
         cls,
         subreddits: Optional[List[str]] = [],
+        flair_filters: Optional[List[str]] = [],
         max_posts_per_subreddit: Optional[int] = -1,
     ) -> bool:
         if subreddits:
             cls.subreddit_list = subreddits
             logger.info(
                 f"Configured ScrapeReddit with subreddits: {cls.subreddit_list}"
+            )
+        if flair_filters:
+            cls.flair_filters = flair_filters
+            logger.info(
+                f"Configured ScrapeReddit with flair filters: {cls.flair_filters}"
             )
         if max_posts_per_subreddit > 5:
             cls.MAX_POSTS_PER_SUBREDDIT = max_posts_per_subreddit
@@ -76,6 +83,15 @@ class ScrapeReddit:
         )
 
     @classmethod
+    def is_valid_flair(cls, flair):
+        if not flair:
+            return True
+        for filter in cls.flair_filters:
+            if filter.upper() in flair.upper():
+                return False
+        return True
+
+    @classmethod
     def __retrieve_posts(cls):
         cls.save_directory
 
@@ -94,26 +110,29 @@ class ScrapeReddit:
         for subred in cls.subreddit_list:
             logger.info(f"Starting to scrape {subred.upper()}")
             subreddit = reddit_instance.subreddit(subred)
-            hot_post = subreddit.hot(limit=cls.MAX_POSTS_PER_SUBREDDIT)
+            hot_posts = subreddit.hot(limit=cls.MAX_POSTS_PER_SUBREDDIT * 2)
 
             subreddit_post_count = 0
-            for sub in hot_post:
-                # TODO: fix this flair filter and extract out to config
-                if not (
-                    sub.link_flair_text in ["Weekend Discussion", "Technical Analysis"]
-                ):
+
+            for post in hot_posts:
+                sub_flair = post.link_flair_text
+
+                if cls.is_valid_flair(sub_flair):
                     subred_list.append(subred)
-                    author_list.append(sub.author)
-                    link_flair_text_list.append(sub.link_flair_text)
-                    num_comments_list.append(sub.num_comments)
-                    score_list.append(sub.score)
-                    title_list.append(sub.title)
-                    upvote_ratio_list.append(sub.upvote_ratio)
+                    author_list.append(post.author)
+                    link_flair_text_list.append(post.link_flair_text)
+                    num_comments_list.append(post.num_comments)
+                    score_list.append(post.score)
+                    title_list.append(post.title)
+                    upvote_ratio_list.append(post.upvote_ratio)
                     subreddit_post_count += 1
+
+                if subreddit_post_count == cls.MAX_POSTS_PER_SUBREDDIT:
+                    break
 
             logger.info(f"Scraped {subreddit_post_count} posts from {subred.upper()}")
 
-        return {
+        cls.__posts = {
             "Subreddit": subred_list,
             "Title": title_list,
             "Count_of_Comments": num_comments_list,
@@ -123,15 +142,37 @@ class ScrapeReddit:
             "Author": author_list,
         }
 
+    @staticmethod
+    def __flairs(posts_df):
+        try:
+            assert not posts_df.empty
+        except AssertionError:
+            logger.warn("No posts to check!")
+            return
+        except AttributeError:
+            logger.error("flairs expect a dataframe!")
+            raise
+        flairs = posts_df["Flair"].unique()
+        logger.info("Flairs:")
+        [logger.info(flair) for flair in flairs]
+
     @classmethod
     def __posts_ETL(cls):
-        posts_df = pd.DataFrame(cls.__retrieve_posts())
+        try:
+            assert cls.__posts
+        except AssertionError:
+            logger.error("No posts scraped yet!")
+            return
+
+        posts_df = pd.DataFrame(cls.__posts)
+        cls.__flairs(posts_df)
         posts_df.to_csv(cls.save_directory / "reddit_dataset.csv", index=False)
 
         dump(posts_df, cls.save_directory / "reddit_dataset.joblib")
 
     @classmethod
     def scrape(cls):
+        cls.__retrieve_posts()
         cls.__posts_ETL()
 
 
@@ -142,6 +183,14 @@ def scrape():
             "ocugen",
             "teslainvestorsclub",
             "SPACs",
+        ],
+        [
+            "Weekend",
+            "Analysis",
+            "Daily",
+            "Discussion",
+            "Thread",
+            "Mods",
         ],
         50,
     )
