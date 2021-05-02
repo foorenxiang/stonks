b3fd35a04d6aab9f535bdc6e2f967f07b34c880b
Foo Ren Xiang <foorenxiang@gmail.com>
2021-05-02 18:05:07 +0800

    refactored autogluon train test module to use paths relative to project root

--- StonksML/autogluon_train_test.py
+++ StonksML/autogluon_train_test.py
@@ -7,38 +7,35 @@ import pandas as pd
 import joblib
 import logging
 from pathlib import Path
+from utils import paths_catalog
 
-file_dir = Path(__file__).resolve().parent
-model_save_path = file_dir / "autogluon_model"
-try:
-    model_save_path.mkdir()
-except FileExistsError:
-    pass
+CURRENT_DIRECTORY = Path(__file__).resolve().parent
+MODEL_SAVE_PATH = paths_catalog.AUTOGLUON_MODEL
+
 
 logging.basicConfig(
-    level=logging.INFO,
-    format="%(asctime)s [%(levelname)s] %(message)s",
-    handlers=[
-        logging.FileHandler(
-            filename=model_save_path / "autogluon_training.log", mode="w"
-        ),
-        logging.StreamHandler(),
-    ],
+    level=logging.INFO, format="%(asctime)s [%(levelname)s] %(message)s"
 )
-logger = logging.getLogger(__file__)
+logger = logging.getLogger(__name__)
+logger.addHandler(logging.StreamHandler())
 
+try:
+    MODEL_SAVE_PATH.mkdir()
+except FileExistsError:
+    logger.info(f"{MODEL_SAVE_PATH} exists, using it")
+
+logger.addHandler(
+    logging.FileHandler(filename=MODEL_SAVE_PATH / "autogluon_training.log", mode="w")
+)
 
 warnings.filterwarnings("ignore")
 np.random.seed(123)
 
-dataset_folder = "datasets"
-dataset_name = "full_preprocessed_reddit_twitter_dataset.joblib"
-dataset_location = (
-    file_dir.resolve().parent / dataset_folder / "preprocessed" / dataset_name
-)
-logger.info(f"Dataset: {dataset_location}")
+DATASET_NAME = "full_preprocessed_reddit_twitter_dataset.joblib"
+DATASET_LOCATION = paths_catalog.PREPROCESSED_DATASETS / DATASET_NAME
+logger.info(f"Dataset: {DATASET_LOCATION}")
 
-dataset_df = joblib.load(dataset_location)[["sentence", "label"]]
+dataset_df = joblib.load(DATASET_LOCATION)[["sentence", "label"]]
 
 num_rows_to_extract = 110400
 num_test_rows = 10000
@@ -53,7 +50,7 @@ logger.info(f"{num_training_rows} rows used for training")
 from autogluon.text import TextPredictor
 
 time_limit_in_secs = 60 * 60 * 15
-predictor = TextPredictor(label="label", eval_metric="acc", path=model_save_path)
+predictor = TextPredictor(label="label", eval_metric="acc", path=MODEL_SAVE_PATH)
 predictor.fit(train_data, time_limit=time_limit_in_secs)
 
 """Evaluation"""
