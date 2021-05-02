aff3c9bb9b89fe35c301a523ae7924478387dad0
Foo Ren Xiang <foorenxiang@gmail.com>
2021-05-02 20:26:37 +0800

    change streamlit module to reference ticker_symbols module in utils

--- StonksStreamlit/app.py
+++ StonksStreamlit/app.py
@@ -1,5 +1,10 @@
-from ticker_symbols import get_ticker_symbols
-from show_tickers import ShowTickers
+import sys
+from from_root import from_root
+
+sys.path.append(str(from_root(".")))
+
+from utils.ticker_symbols import get_ticker_symbols
+from show_tickers import StreamlitShowTickers
 
 if __name__ == "__main__":
-    ShowTickers.show_tickers(get_ticker_symbols())
+    StreamlitShowTickers.show_tickers(get_ticker_symbols())
