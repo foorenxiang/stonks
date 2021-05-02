901c0c15a2df0a4463fecd87b38d5f7e6e67c490
Foo Ren Xiang <foorenxiang@gmail.com>
2021-05-02 18:30:25 +0800

    refactored autogluon train test module to encapsulate training and testing a function

--- StonksML/autogluon_train_test.py
+++ StonksML/autogluon_train_test.py
@@ -11,6 +11,8 @@ from utils import paths_catalog
 
 CURRENT_DIRECTORY = Path(__file__).resolve().parent
 MODEL_SAVE_PATH = paths_catalog.AUTOGLUON_MODEL
+NUM_ROWS_TO_EXTRACT = 110400
+NUM_TEST_ROWS = 10000
 
 
 logging.basicConfig(
@@ -19,40 +21,44 @@ logging.basicConfig(
 logger = logging.getLogger(__name__)
 logger.addHandler(logging.StreamHandler())
 
-try:
-    MODEL_SAVE_PATH.mkdir()
-except FileExistsError:
-    logger.info(f"{MODEL_SAVE_PATH} exists, using it")
 
-logger.addHandler(
-    logging.FileHandler(filename=MODEL_SAVE_PATH / "autogluon_training.log", mode="w")
-)
+def train_test():
+    try:
+        MODEL_SAVE_PATH.mkdir()
+    except FileExistsError:
+        logger.info(f"{MODEL_SAVE_PATH} exists, using it")
 
-warnings.filterwarnings("ignore")
-np.random.seed(123)
+    logger.addHandler(
+        logging.FileHandler(
+            filename=MODEL_SAVE_PATH / "autogluon_training.log", mode="w"
+        )
+    )
 
-DATASET_NAME = "full_preprocessed_reddit_twitter_dataset.joblib"
-DATASET_LOCATION = paths_catalog.PREPROCESSED_DATASETS / DATASET_NAME
-logger.info(f"Dataset: {DATASET_LOCATION}")
+    warnings.filterwarnings("ignore")
+    np.random.seed(123)
 
-dataset_df = joblib.load(DATASET_LOCATION)[["sentence", "label"]]
+    DATASET_NAME = "full_preprocessed_reddit_twitter_dataset.joblib"
+    DATASET_LOCATION = paths_catalog.PREPROCESSED_DATASETS / DATASET_NAME
+    logger.info(f"Dataset: {DATASET_LOCATION}")
 
-num_rows_to_extract = 110400
-num_test_rows = 10000
-num_training_rows = num_rows_to_extract - num_test_rows
-train_data = dataset_df.iloc[:num_training_rows]
-test_data = dataset_df.iloc[-num_test_rows:]
-sample_data = dataset_df.sample(n=1000)
-logger.info(f"{num_training_rows} rows used for training")
+    dataset_df = joblib.load(DATASET_LOCATION)[["sentence", "label"]]
+
+    num_training_rows = NUM_ROWS_TO_EXTRACT - NUM_TEST_ROWS
+    train_data = dataset_df.iloc[:num_training_rows]
+    test_data = dataset_df.iloc[-NUM_TEST_ROWS:]
+    logger.info(f"{num_training_rows} rows used for training")
+
+    """training"""
+    from autogluon.text import TextPredictor
+
+    time_limit_in_secs = 60 * 60 * 15
+    predictor = TextPredictor(label="label", eval_metric="acc", path=MODEL_SAVE_PATH)
+    predictor.fit(train_data, time_limit=time_limit_in_secs)
+
+    """Evaluation"""
+    test_score = predictor.evaluate(test_data, metrics=["acc", "f1"])
+    print(test_score)
 
 
-"""training"""
-from autogluon.text import TextPredictor
-
-time_limit_in_secs = 60 * 60 * 15
-predictor = TextPredictor(label="label", eval_metric="acc", path=MODEL_SAVE_PATH)
-predictor.fit(train_data, time_limit=time_limit_in_secs)
-
-"""Evaluation"""
-test_score = predictor.evaluate(test_data, metrics=["acc", "f1"])
-print(test_score)
+if __name__ == "__main__":
+    train_test()
