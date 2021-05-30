5de5e21d750516e4c8a4736e40f9b10d91252d52
Kang Pyo <kangpyo97@naver.com>
2021-05-30 21:47:38 +0900

    node modules installed and webpack setup

---
+++ react-web/src/index.js
@@ -0,0 +1,6 @@
+import React from "react";
+import { render } from "react-dom";
+
+const Greeting = () => <h1>Hello from React!</h1>;
+
+render(<Greeting />, document.getElementById("root"));
---
+++ react-web/webpack.config.js
@@ -0,0 +1,28 @@
+const path = require("path");
+// const HtmlWebPackPlugin = require("html-webpack-plugin");
+
+module.exports = {
+  entry: "./src/index.js",
+  output: {
+    filename: "main.js",
+    path: path.resolve(__dirname, "public"),
+  },
+  devServer: {
+    contentBase: path.join(__dirname, "public"),
+    port: 9000,
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: ["@babel/preset-env", "@babel/preset-react"],
+          },
+        },
+      },
+    ],
+  },
+};
