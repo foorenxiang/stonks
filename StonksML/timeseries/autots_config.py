90084ae410df2b50e25c46a09e8153c7841c8255
Foo Ren Xiang <foorenxiang@gmail.com>
2021-06-05 17:39:38 +0800

    changed dict key definitions

--- StonksML/timeseries/autots_config.py
+++ StonksML/timeseries/autots_config.py
@@ -3,10 +3,12 @@ import math
 
 
 class AutoTSConfigs:
+
     FAST = "FAST"
     ACCURATE = "ACCURATE"
     GPU = "GPU"
     DEFAULT = "DEFAULT"
+
     FORECAST_LENGTH = 7
 
     __suggested_validation_trials = math.floor(FORECAST_LENGTH / 3)
@@ -14,52 +16,51 @@ class AutoTSConfigs:
         __suggested_validation_trials if __suggested_validation_trials > 2 else 2
     )
 
-    __autots_mode_configs = {
-        "FAST": lambda: AutoTS(
-            forecast_length=AutoTSConfigs.FORECAST_LENGTH,
-            frequency="infer",
-            prediction_interval=0.9,
-            ensemble=None,
-            model_list="superfast",
-            transformer_list="fast",
-            max_generations=5,
-            num_validations=2,
-            validation_method="backwards",
-        ),
-        "ACCURATE": lambda: AutoTS(
-            forecast_length=AutoTSConfigs.FORECAST_LENGTH,
-            frequency="infer",
-            prediction_interval=0.9,
-            ensemble="all",
-            model_list="parallel",
-            transformer_list="fast",
-            max_generations=50,
-            num_validations=AutoTSConfigs.validation_trials,
-            validation_method="backwards",
-        ),
-        "GPU": lambda: AutoTS(
-            forecast_length=AutoTSConfigs.FORECAST_LENGTH,
-            frequency="infer",
-            prediction_interval=0.9,
-            ensemble="auto",
-            model_list="gpu",
-            transformer_list="all",
-            max_generations=50,
-            num_validations=AutoTSConfigs.validation_trials,
-            validation_method="backwards",
-        ),
-        "DEFAULT": lambda: AutoTS(
-            forecast_length=AutoTSConfigs.FORECAST_LENGTH,
-            frequency="infer",
-            prediction_interval=0.9,
-            ensemble="auto",
-            model_list="default",
-            transformer_list="all",
-            max_generations=50,
-            num_validations=AutoTSConfigs.validation_trials,
-            validation_method="backwards",
-        ),
-    }
+    __autots_mode_configs = dict()
+    __autots_mode_configs[FAST] = lambda: AutoTS(
+        forecast_length=AutoTSConfigs.FORECAST_LENGTH,
+        frequency="infer",
+        prediction_interval=0.9,
+        ensemble=None,
+        model_list="superfast",
+        transformer_list="fast",
+        max_generations=5,
+        num_validations=2,
+        validation_method="backwards",
+    )
+    __autots_mode_configs[ACCURATE] = lambda: AutoTS(
+        forecast_length=AutoTSConfigs.FORECAST_LENGTH,
+        frequency="infer",
+        prediction_interval=0.9,
+        ensemble="all",
+        model_list="parallel",
+        transformer_list="fast",
+        max_generations=50,
+        num_validations=AutoTSConfigs.validation_trials,
+        validation_method="backwards",
+    )
+    __autots_mode_configs[GPU] = lambda: AutoTS(
+        forecast_length=AutoTSConfigs.FORECAST_LENGTH,
+        frequency="infer",
+        prediction_interval=0.9,
+        ensemble="auto",
+        model_list="gpu",
+        transformer_list="all",
+        max_generations=50,
+        num_validations=AutoTSConfigs.validation_trials,
+        validation_method="backwards",
+    )
+    __autots_mode_configs[DEFAULT] = lambda: AutoTS(
+        forecast_length=AutoTSConfigs.FORECAST_LENGTH,
+        frequency="infer",
+        prediction_interval=0.9,
+        ensemble="auto",
+        model_list="default",
+        transformer_list="all",
+        max_generations=50,
+        num_validations=AutoTSConfigs.validation_trials,
+        validation_method="backwards",
+    )
 
     @classmethod
     def create_model_lambda(cls, selectedMode):
