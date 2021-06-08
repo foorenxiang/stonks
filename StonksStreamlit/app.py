d63c7a9b2833c051cad3daf1c28d39094b01fa3c
Foo Ren Xiang <foorenxiang@gmail.com>
2021-06-09 06:24:01 +0800

    fixed absolute import

--- StonksStreamlit/app.py
+++ StonksStreamlit/app.py
@@ -4,7 +4,7 @@ from from_root import from_root
 sys.path.append(str(from_root(".")))
 
 from utils.ticker_symbols import get_ticker_symbols
-from show_tickers import StreamlitShowTickers
+from StonksStreamlit.show_tickers import StreamlitShowTickers
 
 if __name__ == "__main__":
     StreamlitShowTickers.show_tickers(get_ticker_symbols())
