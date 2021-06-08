42f5d47b6ecd95d55f9ea2b71f3d4b3059549e57
Foo Ren Xiang <foorenxiang@gmail.com>
2021-06-09 05:47:04 +0800

    refactored streamlit display

--- StonksStreamlit/show_tickers.py
+++ StonksStreamlit/show_tickers.py
@@ -13,30 +13,52 @@ class StreamlitShowTickers:
     tickerSymbols = set()
 
     @classmethod
-    def show_tickers(cls, tickerSymbols={}):
-        if not tickerSymbols:
-            st.write("### Please add ticker symbols to tickerSymbols.py to continue...")
-            return
-
-        stocks_predictions = generate_streamlit_results()
+    def _ml_predictions(cls):
+        # stocks_predictions = generate_streamlit_results()
+        stocks_predictions = None
         if not stocks_predictions:
             st.write(
                 "##### Please wait while we train/load the ML results, check back again later"
             )
 
-        cls.tickerSymbols = tickerSymbols
-        for tickerSymbol in cls.tickerSymbols:
-            tickerData = yf.Ticker(tickerSymbol)
+        return stocks_predictions
 
-            tickerDF = tickerData.history(period="1y")
+    @classmethod
+    def show_tickers(cls, tickerSymbols={}):
+        if not tickerSymbols:
+            st.write("### Please add ticker symbols to tickerSymbols.py to continue...")
+            return
+
+        stocks_predictions = cls._ml_predictions()
+
+        cls.tickerSymbols = set(tickerSymbols)
+
+        cls._display_tickers(stocks_predictions)
+
+    @staticmethod
+    def _retrieve_ticker_data(tickerSymbol):
+        tickerData = yf.Ticker(tickerSymbol)
+
+        tickerDF = tickerData.history(period="1y")
+        return tickerDF
+
+    @classmethod
+    def _display_tickers(cls, stocks_predictions):
+        for tickerSymbol in cls.tickerSymbols:
+            tickerDF = cls._retrieve_ticker_data(tickerSymbol)
 
             if not tickerDF.Close.empty | tickerDF.Volume.empty:
-                st.write(f"## {tickerSymbol}")
-                st.line_chart(tickerDF.Close)
-                st.write(f"{tickerSymbol} Closing Prices")
+                try:
+                    st.write(f"## {tickerSymbol}")
+                    st.line_chart(tickerDF.Close)
+                    st.write(f"{tickerSymbol} Closing Prices")
 
-                st.line_chart(tickerDF.Volume)
-                st.write(f"{tickerSymbol} Volume\n\n\n")
+                    st.line_chart(tickerDF.Volume)
+                    st.write(f"{tickerSymbol} Volume\n\n\n")
+                except TypeError:
+                    st.write(
+                        f"Error encountered with writing full results for {tickerSymbol}"
+                    )
 
             else:
                 st.write(f"{tickerSymbol} is not a valid symbol on Yahoo Finance!!")
